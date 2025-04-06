540d6a0c36d2cf743a6bb4e90752fd842e92931f
ptkrms <log_patrick@hotmail.com>
2025-04-06 08:59:41 +0000

    alteracoes nomes arquivos

---
+++ nivel_intermediario/modulo1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+//codigos para criação de funções
+// Funções para entrada de dados, cálculo da média e exibição da média
+void entradaDados () {
+
+
+}
+
+float calcularMedia() {
+
+
+}
+
+void exibirMedia () {
+
+
+}
+
+//codigo para a chamada das funções
+// Função principal
+int main() {
+    entradaDados();
+    float media = calcularMedia();
+    exibirMedia();
+    return 0;
+}
