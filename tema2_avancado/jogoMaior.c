cfd36637da1b23710686b8ae47b0ed2ac3e193f3
ptkrms <log_patrick@hotmail.com>
2025-05-08 07:16:55 +0000

    novo jogo

--- tema2_avancado/jogoMaior.c
+++ tema2_avancado/jogoMaior.c
@@ -1,2 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main () {
+
+    int numeroJogador, numeroComputador, resultado;
+    char tipoComparacao;
+
+    //gerar numero aleatório
+    srand(time(0));
+    numeroComputador = rand() % 100 + 1; // gera um número entre 1 e 100
+
+    //Inicio do jogo
+    printf("Bem-vindo ao jogo do Maior, Menor ou Igual!\n");
+    printf("Você deve escolher um número e o tipo de comparação.\n");
+    printf("M. Maior\n");
+    printf("N. Menor\n");
+    printf("I. Igual\n");
+
+
+
+
+
+
+
+    return
+}
 
